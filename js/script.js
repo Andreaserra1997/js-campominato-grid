ee8fa359be55501e997ae4b5f954db00bb494c28
Andreaserra1997 <serra.andrea1997@gmail.com>
2023-04-21 16:17:19 +0200

    add btn-play click

--- js/script.js
+++ js/script.js
@@ -4,23 +4,23 @@ Ci saranno quindi 10 caselle per ognuna delle 10 righe.
 Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.*/
 
 const eleGrid = document.querySelector('.grid');
+const btnPlay = document.querySelector('#play');
 
-// generare la griglia
-createGrid(100, eleGrid);
+btnPlay.addEventListener('click',
+    function () {
+        createGrid(100, eleGrid);
+        function createGrid(numCells, eleContainer) {
+            for (let i = 1; i <= numCells; i++) {
+                const cell = document.createElement('div');
+                cell.classList.add('cell');
+                cell.innerHTML = [i];
+                eleContainer.append(cell);
+                cell.addEventListener('click', function () {
+                    console.log(cell);
+                    cell.classList.toggle('clicked');
+                });
+            }
+        };
+    }
+);
 
-
-/* FUNCTION DEFINITIONS */
-
-function createGrid(numCells, eleContainer) {
-	for (let i = 1; i <= numCells; i++) {
-        // eleContainer.innerHTML += `<div class="cell">${i}</div>`;
-        const cell = document.createElement('div');
-        cell.classList.add('cell');
-        cell.innerHTML = [i];
-        eleContainer.append(cell);
-        cell.addEventListener('click', function() {
-			console.log(cell);
-			cell.classList.toggle('clicked');
-		});
-	}
-};
