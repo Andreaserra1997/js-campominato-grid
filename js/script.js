bb5366221077ca95a2c44f5e31b097cfad1de805
Andreaserra1997 <serra.andrea1997@gmail.com>
2023-04-24 13:11:38 +0200

    add bonus

--- js/script.js
+++ js/script.js
@@ -4,13 +4,30 @@ Ci saranno quindi 10 caselle per ognuna delle 10 righe.
 Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.*/
 
 const eleGrid = document.querySelector('.grid');
+const eleCell = document.querySelector('.cell');
 const btnPlay = document.querySelector('#play');
-
-const selectDifficulty = document.querySelector('#difficulty').value;
 
 btnPlay.addEventListener('click',
     function () {
-        createGrid(100, eleGrid);
+        const selectDifficulty = document.querySelector('#difficulty');
+        let valueDifficulty = selectDifficulty.options[selectDifficulty.selectedIndex].value;
+        // const whEasy = 'calc(100% / 10)'
+        // const whHard = 'calc(100% / 9)'
+        // const whCrazy = 'calc(100% / 7)'
+
+        if (valueDifficulty === 'easy') {
+            // eleCell.style.width = whEasy;
+            // eleCell.style.height = whEasy;
+            createGrid(100, eleGrid);
+        } else if (valueDifficulty === 'hard') {
+            // eleCell.style.width = whHard;
+            // eleCell.style.height = whHard;
+            createGrid(81, eleGrid)
+        } else if (valueDifficulty === 'crazy') {
+            // eleCell.style.width = whCrazy;
+            // eleCell.style.height = whCrazy;
+            createGrid(49, eleGrid)
+        }
     }
 );
 
